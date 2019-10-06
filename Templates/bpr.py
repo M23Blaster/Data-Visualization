caf9d52b0d1a9b08b31746e4add65fe16a4808da
James Weichert <james.p.weichert@gmail.com>
2019-10-05 21:43:14 -0700

    Added Line Plot, Scatter Plot, and Bar Chart Templates

--- Templates/bpr.py
+++ Templates/bpr.py
@@ -22,8 +22,8 @@ import plotly.graph_objects as go
 #===Partisan Data Color Scales===
 
 dem_rep = ["#487EBF",  "#BF424A"]
-safe_tossup = ["#444CA0", "#F2DDA9", "#A04555"]
 safe_leans_tossup = ["#444CA0", "#81B6E8", "#F2DDA9", "#DD758A", "#A04555"]
+safe_tossup = ["#444CA0", "#F2DDA9", "#A04555"]
 safe_likely_leans_tossup = ["#444CA0", "#487EBF", "#81B6E8", "#F2DDA9", "#DD758A", "#BF424A", "#A04555"]
 
 
@@ -45,6 +45,46 @@ def create_us_state_map(data, z_col, state_col='State', map_title="[MAP TITLE]",
         title_text = map_title,
         geo_scope = 'usa',
         dragmode = False
+    )
+    
+    return fig
+
+def create_barchart(data, x, y, chart_title="[CHART TITLE]", labels_dict={}, c=["indianred"]):
+    fig = px.bar(
+        data,
+        x = x,
+        y = y,
+        title = chart_title,
+        labels = labels_dict,
+        color_discrete_sequence = c
+    )
+    
+    fig.show()
+    
+    return fig
+
+def create_histogram(data, column, n_bins=10, chart_title="[CHART TITLE]", labels_dict={}, c=["indianred"]):
+    fig = px.histogram(
+        data,
+        column,
+        nbins = n_bins,
+        title = chart_title,
+        labels = labels_dict,
+        color_discrete_sequence = c
+    )
+    
+    fig.show()
+    
+    return fig
+
+def create_scatter_plot(data, x, y, chart_title="[CHART TITLE]", labels_dict={}, c=["indianred"]):
+    fig = px.scatter(
+        data,
+        x = x,
+        y = y,
+        title = chart_title,
+        labels = labels_dict,
+        color_discrete_sequence = c
     )
     
     fig.show()
